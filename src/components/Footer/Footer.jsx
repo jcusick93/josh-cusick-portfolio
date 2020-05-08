6a05c67c4ea1c498710865b7af7280918c640e36
Josh Cusick <joshcusick@MININT-0GM8GG0.europe.corp.microsoft.com>
2020-05-08 11:47:27 -0700

    updated thumbnail

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,20 @@ import "./Footer.css";
 
 const Footer = () => (
   <div className="footer">
-    <span>© 2020 designed & coded in React.js by yours truly</span>
+    <div className="status-copyright-container">
+      <a
+        href="https://app.netlify.com/sites/josh-cusick-portfolio/deploys"
+        className="status-container"
+      >
+        <img
+          alt="status badge"
+          src="https://api.netlify.com/api/v1/badges/71d93d8c-14d1-421c-ac33-4c8a76636e1c/deploy-status"
+        />
+      </a>
+
+      <span>© 2020 Josh Cusick 💅🏻 in Figma + 🛠 in React</span>
+    </div>
+
     <div className="social-container">
       <a className="social-link" href="/">
         LinkedIn
