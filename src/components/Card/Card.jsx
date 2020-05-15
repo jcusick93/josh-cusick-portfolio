5a444afc1609d5cd59b4e95dd332cefcd59a46eb
Josh Cusick <joshcusick@tiasSurflaptop.redmond.corp.microsoft.com>
2020-05-14 18:49:17 -0700

    updated card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,33 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./Card.css";
 import { Badge } from "../Badge/Badge";
 import Image from "../Image/Image";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 function Card(props) {
   return (
-    <SkeletonTheme
-      color="var(--skeleton-background)"
-      highlightColor="var(--skeleton-highlight)"
-    >
-      <div className="card">
-        {props.children}
+    <a href="/compass-design-system" className="card">
+      {props.children}
 
-        <Badge varient="website" margin={16} badgeText={props.badgeText} />
-
-        <Image height="24vh" image={props.image} />
-        {/* <Skeleton height="32vh" /> */}
-        <div className="card-title">
-          <h1>{props.title || <Skeleton width={140} />}</h1>
-          <span>{props.content || <Skeleton />}</span> <br /> <br />
-          {/* <Skeleton float="left" width={120} /> */}
-          <NavLink className="card-link" to="./compass-design-system">
-            {props.linkText} →
-          </NavLink>
-        </div>
+      <Badge varient="website" margin={16} badgeText={props.badgeText} />
+      <Image height="24vh" image={props.image} />
+      <div className="card-title">
+        <h1>{props.title}</h1>
+        <span>{props.content}</span>
       </div>
-    </SkeletonTheme>
+    </a>
   );
 }
 
